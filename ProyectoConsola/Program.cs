b07d9124c0c27314ab1454575a179cc5dc83580a
LeydaJrz <ljuarez@cibergestion.com.mx>
2023-08-07 20:42:12 -0600

    prueba consola

--- ProyectoConsola/Program.cs
+++ ProyectoConsola/Program.cs
@@ -3,4 +3,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 2!");
 
 Console.WriteLine("Hello, Req5!");
+Console.WriteLine("Hello, Req6!");
+Console.WriteLine("Hello, Req7!");
 
